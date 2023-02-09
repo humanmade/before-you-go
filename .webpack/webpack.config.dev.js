c8dd041370a52309d37835705ce927acc3dcab70
K. Adam White <kadamwhite@users.noreply.github.com>
2023-02-09 13:24:29 -0500

    Add basic plugin build code and tooling

---
+++ .webpack/webpack.config.dev.js
@@ -0,0 +1,20 @@
+const { externals, helpers, presets } = require( '@humanmade/webpack-helpers' );
+
+const { choosePort, cleanOnExit, filePath } = helpers;
+
+cleanOnExit( [
+	filePath( 'build', 'development-asset-manifest.json' ),
+] );
+
+module.exports = choosePort( 9090 ).then( ( port ) => presets.development( {
+	name: 'before-you-go',
+	devServer: {
+		server: 'https',
+		port,
+	},
+	externals,
+	entry: {
+		'before-you-go-editor': filePath( 'src/editor.js' ),
+		'before-you-go-frontend': filePath( 'src/frontend.js' ),
+	},
+} ) );
---
+++ .webpack/webpack.config.prod.js
@@ -0,0 +1,18 @@
+const { externals, helpers, plugins, presets } = require( '@humanmade/webpack-helpers' );
+
+const { filePath } = helpers;
+
+module.exports = presets.production( {
+    name: 'before-you-go',
+    externals,
+    entry: {
+        'before-you-go-editor': filePath( 'src/editor.js' ),
+        'before-you-go-frontend': filePath( 'src/frontend.js' ),
+    },
+    plugins: [
+        plugins.clean(),
+    ],
+	cache: {
+		type: 'filesystem',
+	},
+} );
