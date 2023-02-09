4a5e3a1ba09394080c0f81d6e17aab0f92458e90
K. Adam White <kadamwhite@users.noreply.github.com>
2023-02-09 13:44:30 -0500

    Add (empty) initial plugin source code files

---
+++ inc/namespace.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Coordinate top-level initialization of the plugin and its modules.
+ *
+ * @package before-you-go
+ */
+
+declare( strict_types=1 );
+
+namespace Before_You_Go;
+
+/**
+ * Connect namespace functions to actions and hooks.
+ */
+function bootstrap() : void {
+	// TK
+}
---
+++ plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Before You Go
+ * Description: Editorial tool to show offers or recirculation prompts as a reader exits the site.
+ * Author: Human Made
+ * Author URI: https://humanmade.com
+ * Version: 0.0.1
+ */
+
+namespace Before_You_Go;
+
+require_once __DIR__ . '/inc/namespace.php';
+bootstrap();
