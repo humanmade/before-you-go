83e2ef3e38f92e403f5ffc6cbcc1e273b33a7201
K. Adam White <kadamwhite@users.noreply.github.com>
2023-02-12 21:57:12 -0500

    Make BYG page queryable and improve admin-side display
    - Enable block editor by exposing to REST
    - Fix typo in icon to use
    - Make BYG items viewable on the frontend
    
    POST DEPLOY ACTION: Flush rewrites

--- inc/post-types/byg-page.php
+++ inc/post-types/byg-page.php
@@ -35,29 +35,34 @@ function register_type() {
 		'search_items'        => esc_html__( 'Search Before You Go pages', 'byg-admin' ),
 		'not_found'           => esc_html__( 'No Before You Go page found', 'byg-admin' ),
 		'not_found_in_trash'  => esc_html__( 'No Before You Go page found in Trash', 'byg-admin' ),
-		'menu_name'           => esc_html__( 'Before You Go pages', 'byg-admin' ),
+		'menu_name'           => esc_html__( 'Before You Go', 'byg-admin' ),
 	];
 
 	$args = [
 		'labels'              => $labels,
 		'hierarchical'        => false,
 		'description'         => 'Before You Go pages are used to display customizable content to users leaving the site after visiting from social media.',
-		'public'              => false,
+		'public'              => true,
 		'show_ui'             => true,
 		'show_in_menu'        => true,
-		'show_in_admin_bar'   => true,
-		'menu_position'       => 5,
-		'menu_icon'           => 'dashicons-back',
+		'show_in_admin_bar'   => false,
+		'show_in_rest'        => true, // Enables block editor.
+		'menu_icon'           => 'dashicons-arrow-left',
 		'show_in_nav_menus'   => false,
-		'publicly_queryable'  => false,
+		'publicly_queryable'  => true,
 		'exclude_from_search' => true,
 		'has_archive'         => false,
-		'query_var'           => POST_TYPE,
+		'query_var'           => 'byg',
 		'capability_type'     => 'post',
 		'supports'            => [
 			'title',
 			'custom-fields',
 			'editor',
+		],
+		'rewrite' => [
+			'slug'       => 'byg',
+			'with_front' => false,
+			'pages'      => false,
 		],
 		/**
 		 * Permit filtering the list of taxonomies added to the BYG Page post types.
