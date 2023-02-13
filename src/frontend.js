4653095780fe43fd6cab8f9ccb5981ae65e2a0d1
K. Adam White <kadamwhite@users.noreply.github.com>
2023-02-12 21:29:14 -0500

    Refactor frontend script to permit injecting custom trigger logic

--- src/frontend.js
+++ src/frontend.js
@@ -1,14 +1,50 @@
 import { has as hasQuery } from './services/query';
 import { has as hasReferrer } from './services/referrer';
 
+/**
+ * BYG global object, mostly PHP-filterable.
+ *
+ * @typedef {object} BYG
+ * @property {string[]} referrers  Array of referrer trigger domains.
+ * @property {string[]} utmSources Array of utm_source trigger values.
+ * @property {boolean}  debug      Whether in debug mode.
+ * @property {Function} [trigger]  Trigger logic for BYG behavior.
+ * @property {Function} [callback] Callback to execute if trigger condition is met.
+ */
+
+/**
+ * Default trigger logic, if no other JS has configured a custom path.
+ *
+ * @param {BYG} BYG Before You Go window global object.
+ */
+const defaultTrigger = ( { referrers, utmSources, callback } ) => {
+	if ( hasQuery( 'utm_source', utmSources ) || hasReferrer( referrers ) ) {
+		callback();
+	}
+};
+
+/**
+ * Default callback to fire when BYG trigger condition is met.
+ */
+const defaultCallback = () => {
+	alert( 'BYG would initialize!' );
+};
+
 window.addEventListener( 'load', () => {
-	// Read in the BYG global values; ensure required arrays are present.
+	// Read in the BYG global values and ensure required arrays are present.
+	/** @type {BYG} */
 	const BYG = Object.assign( {
 		referrers: [],
 		utmSources: [],
 	}, window.BYG || {} );
 
-	if ( hasQuery( 'utm_source', BYG.utmSources ) || hasReferrer( BYG.referrers ) ) {
-		alert( 'BYG would initialize!' );
+	// Ensure trigger and callback are callable.
+	if ( typeof BYG.trigger !== 'function' ) {
+		BYG.trigger = defaultTrigger;
 	}
+	if ( typeof BYG.callback !== 'function' ) {
+		BYG.callback = defaultCallback;
+	}
+
+	BYG.trigger( BYG );
 } );
